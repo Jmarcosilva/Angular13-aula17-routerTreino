eee65ba6e0424c65dbcbd388794cfb7922a15d2e
Jmarcosilva <marcuz.silva@hotmail.com>
2023-07-29 20:59:19 -0300

    Primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavegadorComponent } from './componentes/home/navegador.component';
+import { QuemSomosComponent } from './componentes/quem-somos/quem-somos.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path:"home", component: NavegadorComponent},
+    {path:"quemsomos", component: QuemSomosComponent},
+    {path:"contato", component: ContatoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegadorComponent } from './componentes/home/navegador.component';
+import { QuemSomosComponent } from './componentes/quem-somos/quem-somos.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegadorComponent,
+    QuemSomosComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
